d1977db1e69c508c2965a6b6c1dd48614db224d3
jing <shajingyuan@me.com>
2025-07-25 13:39:10 +0200

    add mp4 file

--- src/patterns/continuity/continuity_chain.py
+++ src/patterns/continuity/continuity_chain.py
@@ -81,7 +81,9 @@ def generate_continuity_chain_task_batch(cs, cc, cz, obj_size, obj_count, base_d
         gif_path = pos_dir / f"{i:05d}.gif"
         mp4_path = pos_dir / f"{i:05d}.mp4"
         imageio.mimsave(str(gif_path), images, duration=0.1)
-        imageio.mimsave(str(mp4_path), images, fps=10, macro_block_size=None, plugin='ffmpeg')
+        with imageio.get_writer(str(mp4_path), 'ffmpeg', fps=10, macro_block_size=None) as writer:
+            for img in images:
+                writer.append_data(img)
 
     for i in range(num_per_class):
         out_dir = neg_dir / f"{i:05d}"
@@ -91,7 +93,9 @@ def generate_continuity_chain_task_batch(cs, cc, cz, obj_size, obj_count, base_d
         gif_path = neg_dir / f"{i:05d}.gif"
         mp4_path = neg_dir / f"{i:05d}.mp4"
         imageio.mimsave(str(gif_path), images, duration=0.1)
-        imageio.mimsave(str(mp4_path), images, fps=10, macro_block_size=None, plugin='ffmpeg')
+        with imageio.get_writer(str(mp4_path), 'ffmpeg', fps=10, macro_block_size=None) as writer:
+            for img in images:
+                writer.append_data(img)
 
     combine_gifs(pos_dir, neg_dir, base_path / "combined.gif")
 
